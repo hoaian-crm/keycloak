4b19531a0d7fe2a81fd5e4bf4e0054bf5629f452
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-02-12 10:03:13 +0700

    Update build package

--- src/modules/example/example.module.ts
+++ src/modules/example/example.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ExampleController } from './Example.controller';
+import { ExampleController } from './example.controller';
 import { TestController } from './test.controller';
 
 @Module({
   controllers: [ExampleController, TestController],
 })
-export class ExampleModule {}
+export class ExampleModule { }
